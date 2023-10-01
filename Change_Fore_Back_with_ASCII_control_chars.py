20b64dd03fc85921fd9630ee942a17e8995a1a99
caribbian <jonson666j@gmail.com>
2023-10-02 01:48:25 +0300

    Add files via upload

---
+++ Change_Fore_Back_with_ASCII_control_chars.py
@@ -0,0 +1,14 @@
+from colorama import init, Fore, Back, Style
+
+init()
+
+print(Fore.WHITE+Back.BLUE+'Скажи-ка, дядя, ведь не даром')
+
+print('Москва, спаленная пожаром,')
+
+print('Французу отдана.')
+
+print()
+
+print('М. Ю. Лермонтов'+Style.RESET_ALL)
+
