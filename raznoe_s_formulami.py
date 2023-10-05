6885325471f25378a566b96e3c6d21f646dd146a
caribbian <jonson666j@gmail.com>
2023-10-05 19:42:29 +0300

    Add files via upload

---
+++ raznoe_s_formulami.py
@@ -0,0 +1,61 @@
+
+# from sys import stdout
+# a, b, c = map(int, input().split())
+# stdout.write(str(a+b+c))
+
+#print('%.2f' % (float(input())*61.0943))
+
+# import math
+# x = float(input())
+# y = math.sqrt(1 / 5 + (math.e ** x) ** (1 / 5))/abs(math.log(x ** 2) - 1.3)
+# print('%.3f' % y)
+
+# from sys import stdout
+# stdout.write(str((float(input())/1000)))
+
+# from sys import stdout
+# usd = 61.0943
+# x = float(input())
+# rub = x * usd
+# stdout.write(str('%.2f' % rub))
+
+# from sys import stdout
+# pi = 3.14
+# r = float(input())
+# v = 4 / 3 * pi * r ** 3
+# stdout.write(str('%.2f' % v))
+
+# from sys import stdout
+# a, h = map(float, input().split())
+# stdout.write(str('%d' % (1/2*a*h)))
+
+# from sys import stdout
+# a1, an, n = map(float, input().split())
+# s=(a1+an)/2*n
+# stdout.write(str('%d' % s))
+
+# import math
+# x = float(input())
+# pi=3.14
+# y = math.sqrt(math.e ** (2.2 * x)) - abs(math.sin(pi * x / (x + 2 / 3))) + 1.7
+# print('%.3f' % y)
+
+# import math
+# x = float(input())
+# y = 1/math.sqrt(x) + math.log(abs(0.2 + math.sin(x))) ** 2 * (x ** 2) ** (1/3)
+# print('%.4f' % y)
+
+# import math
+# x = float(input())
+# y=math.sqrt(x-1)+x**3*math.log(10)
+# print('%.2f' % y)
+
+# import math
+# x = float(input())
+# y=x-30*math.sin(30) + math.sqrt(125)+x
+# print('%.2f' % y)
+
+# import math
+# x = float(input())
+# y=math.cos(30)/math.sqrt(121)+x**2/x
+# print('%.2f' % y)
